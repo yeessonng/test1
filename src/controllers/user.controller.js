0e50dcf4e136cb9920f2d82dae10e937a3a7ec84
yeessonng <dorlek856@gmail.com>
2024-02-06 02:12:46 +0900

    test db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,5 +5,4 @@ import {userTestTest} from '../services/user.service.js';
 //테스트 api
 export const userTest = async (req, res, next) => {
     res.send(response(status.SUCCESS, await userTestTest(req.body)));
-
 }
