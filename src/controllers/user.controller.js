ee13a81090c3d1013c3476d73fcbae88f46d155c
mooinida <120772200+mooinida@users.noreply.github.com>
2024-02-15 15:57:52 +0900

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,5 +7,5 @@ export const userLogin = async (req, res, next) => {
     console.log("로그인을 요청하였습니다!");
     console.log("body:", req.body); // 값이 잘 들어오나 찍어보기 위한 테스트용
 
-    res.send(response(status.SUCCESS, await loginUser(req.body)));
+    //res.send(response(status.SUCCESS, await loginUser(req.body)));
 }
